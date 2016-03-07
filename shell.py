a092c4da9a4254188a011b596dc09693c54d01f9
Alex Egger <alex.egger96@gmail.com>
2016-03-07 22:02:35 +0100

    Add command history and jobs

--- shell.py
+++ shell.py
@@ -2,9 +2,61 @@ import curses
 from curses import cbreak, echo, endwin, initscr, nocbreak, noecho
 import logging
 from os import environ
+from subprocess import check_output, CalledProcessError
 from time import sleep
 from threading import Thread
 from queue import Queue
+
+
+class Job(Thread):
+    __nextId = 0
+
+    def __init__(self, shell, command):
+        super(Job, self).__init__()
+        self.id = Job.__nextId
+        Job.__nextId += 1
+
+        self.__shell = shell
+        self.__command = command
+        self.result = ""
+
+    def run(self):
+        try:
+            self.result = check_output(self.__shell.history.last().split())
+            logging.info("Job {0} has ended.".format(self.id))
+            self.__shell.writer.add(self.result.decode("utf-8"))
+        except FileNotFoundError as e:
+            self.__shell.writer.add("{0}\n".format(str(e)))
+        except CalledProcessError as e:
+            self.__shell.writer.add("{0}\n".format(str(e)))
+
+
+class History:
+
+    def __init__(self):
+        self.__commands = []
+        self.__selectedIndex = 0
+
+    def add(self, command):
+        self.__commands.append(command)
+        self.__selectedIndex = len(self.__commands)
+
+    def previous(self):
+        if self.__selectedIndex - 1 >= 0:
+            self.__selectedIndex -= 1
+            return self.__commands[self.__selectedIndex]
+        return None
+
+    def next(self):
+        if self.__selectedIndex + 1 < len(self.__commands):
+            self.__selectedIndex += 1
+            return self.__commands[self.__selectedIndex]
+        return None
+
+    def last(self):
+        if len(self.__commands) != 0:
+            return self.__commands[-1]
+        return None
 
 
 class Shell:
@@ -12,34 +64,40 @@ class Shell:
     def __init__(self):
         self.__initEnvironment()
         self.__initConfig()
+        self.__initHistory()
         self.__initLogging()
         self.__initWindow()
 
+        self.__jobs = []
+
         logging.info("Starting writer...")
-        self.writer = Shell.Writer(self.window)
+        self.writer = Shell.Writer(self.__window)
         self.writer.start()
 
     def __initConfig(self):
-        self.config = {}
+        self.__config = {}
 
-        prompt = "{0} $".format(self.environment["HOME"])
-        self.config["PROMPT"] = prompt.replace(
-            self.environment["HOME"], "~")
+        prompt = "{0} $".format(self.__environment["HOME"])
+        self.__config["PROMPT"] = prompt.replace(
+            self.__environment["HOME"], "~")
 
     def __initEnvironment(self):
-        self.environment = {}
+        self.__environment = {}
 
-        self.environment["HOME"] = environ.get("HOME")
-        self.environment["PWD"] = environ.get("PWD")
+        self.__environment["HOME"] = environ.get("HOME")
+        self.__environment["PWD"] = environ.get("PWD")
+
+    def __initHistory(self):
+        self.history = History()
 
     def __initLogging(self):
         logging.basicConfig(filename="debug.log", level=logging.DEBUG)
 
     def __initWindow(self):
-        self.window = initscr()
+        self.__window = initscr()
         noecho()
         cbreak()
-        self.window.keypad(True)
+        self.__window.keypad(True)
 
     def __deinitWriter(self):
         logging.info("Stopping writer...")
@@ -49,14 +107,15 @@ class Shell:
     def __deinitWindow(self):
         logging.info("Destructing window...")
         nocbreak()
-        self.window.keypad(False)
+        self.__window.keypad(False)
         echo()
 
     def run(self):
         try:
             while True:
-                self.writer.add(self.config["PROMPT"])
-                self.fetch()
+                self.writer.add("\r{0}".format(self.__config["PROMPT"]))
+                self.__fetch()
+                self.__execute()
         except Exception as e:
             logging.info(e)
         finally:
@@ -64,21 +123,41 @@ class Shell:
             self.__deinitWindow()
             logging.info("Application closing.")
 
-    def fetch(self):
+    def __fetch(self):
         command = ""
 
         while True:
-            character = self.window.getch()
+            character = self.__window.getch()
 
             if character == curses.KEY_ENTER or character == 10:
-                command += "\n"
-                break
+                if len(command.strip()) != 0:
+                    command += "\n"
+                    break
             elif character == curses.KEY_BACKSPACE:
                 command = command[0:-1]
             else:
                 command += chr(character)
-            self.writer.add("\r{0}{1}".format(self.config["PROMPT"], command))
+            self.writer.add("\r{0}{1}".format(
+                self.__config["PROMPT"], command))
+
         self.writer.add("\n")
+        self.history.add(command.strip())
+
+    def __execute(self):
+        # TODO: Check whether to execute in background.
+        command = self.history.last()
+        executeBackground = False
+        if command.split()[-1] == "&":
+            executeBackground = True
+
+        self.__jobs.append(Job(self, command))
+        self.__jobs[-1].start()
+
+        if not executeBackground:
+            self.__jobs[-1].join()
+        else:
+            logging.info(
+                "Started job {0} in background...".format(self.jobs[-1].id))
 
     class Writer(Thread):
 
@@ -109,41 +188,42 @@ class Shell:
 
         def __init__(self, window):
             super(Shell.Writer, self).__init__()
-            self.cursor = Shell.Writer.Cursor(0, 0)
-            self.stopCalled = False
+            self.__cursor = Shell.Writer.Cursor(0, 0)
+            self.__stopCalled = False
 
-            self.queue = Queue()
-            self.window = window
+            self.__queue = Queue()
+            self.__window = window
 
         def add(self, message):
             if message is not None:
-                logging.info("Added the message: {0}".format(message))
-                self.queue.put(message)
+                self.__queue.put(message)
 
         def run(self):
-            while not self.stopCalled:
-                logging.info("Queue empty")
-                if not self.queue.empty():
-                    message = self.queue.get()
-                    logging.info("Print: {0}".format(message))
+            while not self.__stopCalled:
+                if not self.__queue.empty():
+                    message = self.__queue.get()
                     self.__print(message)
-                    self.window.move(self.cursor.y, self.cursor.x)
-                    self.window.refresh()
+                    self.__window.move(self.__cursor.y, self.__cursor.x)
+                    self.__window.refresh()
             sleep(0.1)
 
         def stop(self):
-            self.stopCalled = True
+            self.__stopCalled = True
 
         def __print(self, message):
+            # TODO: Add scrolling.
+
             for character in message:
                 if character == "\n":
-                    self.cursor.reset()
-                    self.cursor.down()
+                    self.__cursor.reset()
+                    self.__cursor.down()
                 elif character == "\r":
-                    self.cursor.reset()
+                    self.__cursor.reset()
+                    self.__window.clrtoeol()
                 else:
-                    self.window.addch(self.cursor.y, self.cursor.x, character)
-                    self.cursor.right()
+                    self.__window.addch(
+                        self.__cursor.y, self.__cursor.x, character)
+                    self.__cursor.right()
 
 if __name__ == "__main__":
     sh = Shell()
